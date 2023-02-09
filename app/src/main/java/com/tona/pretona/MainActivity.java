5aaade8804e1f2b11be73c52e092e1f53aa3fd96
SunethUdayanga <sunethcreation44@gmail.com>
2023-02-09 23:30:30 +0530

    Fix SDK
    Update sdk 32 to 33
    Add dependencies
    Write data

--- app/src/main/java/com/tona/pretona/MainActivity.java
+++ app/src/main/java/com/tona/pretona/MainActivity.java
@@ -4,12 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
 
 
     }
